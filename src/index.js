78bc0dcf2288d57b4eda19262c50fd04cc8bdee9
Nozeil <nozeilwrk@gmail.com>
2022-01-30 16:01:30 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let stack = [];
+	let lastStackItem;
+	let closeBracket;
+	str = str.split('');
+	for (let i = 0; i <= str.length; i++) {
+		if (bracketsConfig.some(config => str[i] === config[0] && !stack.includes(config[1]))) stack.push(str[i]);
+		else if (bracketsConfig.some(config => str[i] === config[1])) {
+			if (stack.length === 0) return false;
+			lastStackItem = stack[stack.length - 1];
+			closeBracket = str[i];
+			if (bracketsConfig.some(config => config[0] === lastStackItem && config[1] === closeBracket)) stack.pop();
+		}
+	}
+	return !stack.length;
 }
